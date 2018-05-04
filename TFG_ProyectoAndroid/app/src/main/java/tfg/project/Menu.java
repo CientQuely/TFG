681079663644b565cbf4b9e53447e663b045eeb0
Unknown <miquel.ramis95@gmail.com>
2018-05-04 19:50:23 +0200

    Menu Background
    Background funcionando, sin botones y con barra de titulo oculta.

---
+++ TFG_ProyectoAndroid/app/src/main/java/tfg/project/Menu.java
@@ -0,0 +1,5 @@
+package tfg.project;
+
+public class Menu {
+
+}
--- TFG_ProyectoAndroid/app/src/main/java/tfg/project/ProjectView.java
+++ TFG_ProyectoAndroid/app/src/main/java/tfg/project/ProjectView.java
@@ -1,22 +1,33 @@
 package tfg.project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import tfg.project.presenter.Presenter;
 import tfg.project.presenter.ProjectPresenter;
 import tfg.project.view.View;
 import tfg.proyecto.R;
 
-public class ProjectView extends AppCompatActivity implements View {
+public class ProjectView extends Activity implements View {
 
     private Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_proyecto);
+
+        //Escondemos el título
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Escondemos la barra de notificaciones
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.menu);
 
         presenter = new ProjectPresenter(this);
     }
+
+
 }
