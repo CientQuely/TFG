d159767a6938b744d4d681444ba942cb8527a908
Unknown <miquel.ramis95@gmail.com>
2018-05-07 19:48:41 +0200

    Visualizacion de camera + Permiso Camara

--- TFG_ProyectoAndroid/app/src/main/java/tfg/project/view/AugmentedReality.java
+++ TFG_ProyectoAndroid/app/src/main/java/tfg/project/view/AugmentedReality.java
@@ -1,32 +1,130 @@
 package tfg.project.view;
 
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import android.hardware.camera2.*;
+
+import java.sql.Time;
+
+import tfg.project.presenter.Presenter;
+import tfg.project.presenter.ProjectPresenter;
+import tfg.project.view.view_exception.CAMERA_NOT_FOUND_EXCEPTION;
 import tfg.proyecto.R;
-import tfg.project.presenter.*;
 
 public class AugmentedReality extends Activity implements View{
 
-   Presenter presentador;
+   private Presenter presentador;
 
+
+   private Camera camara;
+   private CameraView vistaCamara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (!obtainPermission()){
+            Toast.makeText(this, "Esta app no puede funcionar si no se aceptan" +
+                            "todos los permisos.",
+                    Toast.LENGTH_LONG).show();
+        }
 
-        //Escondemos el título
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //Escondemos la barra de notificaciones
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        try {
+            createCameraView();
+
+        } catch (CAMERA_NOT_FOUND_EXCEPTION c) {
+            Toast.makeText(this, "Error: Camera not found in this device",
+                    Toast.LENGTH_LONG).show();
+        }
+
+
 
         setContentView(R.layout.activity_augmented_reality);
+
+        FrameLayout frame = findViewById(R.id.camera_view);
+        frame.addView(vistaCamara);
 
         presentador = new ProjectPresenter(this);
     }
 
+    private boolean obtainPermission() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+
+           if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                   Manifest.permission.CAMERA)) {
+
+           }else{
+
+               ActivityCompat.requestPermissions(this,
+                       new String[]{Manifest.permission.CAMERA}, 0
+                       );
+
+           }
+        }
+        return true;
+    }
+
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    private void createCameraView() throws CAMERA_NOT_FOUND_EXCEPTION{
+
+        if(!checkCamera(getApplicationContext())) throw new CAMERA_NOT_FOUND_EXCEPTION();
+
+        camara = obtainCamera();
+                if(camara == null ){
+            Toast.makeText(this, "Error: Camera is in use by other program",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        vistaCamara = new CameraView(this, camara);
+    }
+
+    /** Comprueba que la cámara existe*/
+    private boolean checkCamera(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // tiene cámara
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Obtiene una instancia de la cámara */
+    public static Camera obtainCamera() throws RuntimeException{
+        Camera c = null;
+        try {
+            //Obtiene el control de la cámara trasera del dispositivo
+            c = Camera.open(0);
+        }
+        catch (RuntimeException re){
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /**
+     * Libera la cámara del control de esta app
+     */
+    private void releaseCamera(){
+        if (camara != null){
+            camara.release();
+            camara = null;
+        }
+    }
 
 }
---
+++ TFG_ProyectoAndroid/app/src/main/java/tfg/project/view/view_exception/CAMERA_NOT_FOUND_EXCEPTION.java
@@ -0,0 +1,7 @@
+package tfg.project.view.view_exception;
+
+public class CAMERA_NOT_FOUND_EXCEPTION extends Exception {
+    public CAMERA_NOT_FOUND_EXCEPTION(){
+        super();
+    }
+}
